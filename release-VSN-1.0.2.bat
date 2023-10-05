db7a25c356f11a3388cf9fc8021fd1c23dc6cfd0
Cesr Castano <castanc@gmail.com>
2023-10-05 18:10:42 -0300

    VSN-1.0.3.0

--- release-VSN-1.0.2.bat
+++
@@ -1,8 +0,0 @@
-cls
-REM BaseApplication2
-REM RELEASE VSN-1.0.2.33 10/5/2023 5:52:12 PM
-P:
-CD P:\MainApps\BaseApplication2\VSN-1.0.2
-git add .
-git commit -m "VSN-1.0.2.33"
-git push
--- release-VSN-1.0.3.bat
+++ release-VSN-1.0.3.bat
@@ -1,8 +1,10 @@
 cls
 REM BaseApplication2
-REM RELEASE VSN-1.0.3.0 10/5/2023 6:04:52 PM
+REM RELEASE VSN-1.0.3.0 10/5/2023 6:10:38 PM
 P:
 CD P:\MainApps\BaseApplication2\VSN-1.0.3
+git checkout VSN-1.0.3
 git add .
 git commit -m "VSN-1.0.3.0"
 git push
+exit
