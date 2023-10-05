9542570822005160b9e50f21953d704d1cd1ec8c
Cesr Castano <castanc@gmail.com>
2023-10-05 18:28:33 -0300

    VSN-1.0.5

---
+++ new-release.bat
@@ -0,0 +1,6 @@
+P:
+CD P:\MainApps\BaseApplication2\VSN-1.0.5
+git add .
+git commit -m "VSN-1.0.5"
+git push --set-upstream origin VSN-1.0.5
+exit
---
+++ pre-release.bat
@@ -0,0 +1,4 @@
+P:
+CD P:\MainApps\BaseApplication2\VSN-1.0.5
+git checkout -b VSN-1.0.5
+exit
